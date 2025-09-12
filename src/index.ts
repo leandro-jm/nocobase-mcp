af468bcf370f461287a17deb1dc63c12d0d6aa63
leandro.jm <leandro.jm@gmail.com>
2025-09-11 22:01:18 -0300

    Changes

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,9 @@ import z from "zod";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
-// recria __dirname no ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// agora funciona
 dotenv.config({ path: __dirname + '/../.env' });
 
 const API_BASE = process.env.API_BASE;
@@ -25,9 +23,10 @@ const server = new McpServer({
  * Type definition for the Foods data structure.
  * Represents the structure of food items returned by the API.
  */
-type Foods = {
+type Product = {
   data?: {
     id?: number;
+    category?: string;
     title?: string;
     description?: string;
     price?: string;
@@ -90,34 +89,36 @@ async function makeRequest<T>(
 }
 
 /**
- * Tool to fetch all available food items registered in the CRM.
+ * Tool to list all food items available for a given ZIP code (CEP).
+ * It requires the CEP to be specified.
  */
 server.tool(
-  "Alimentos-Tool",
-  "Busca todos os alimentos disponíveis para venda cadastrado no crm para o CEP informado.",
+  "Produtos-Tool",
+  "Busca todos os produtos disponíveis para o CEP informado.",
   {
     cep: z.string().trim(),
   },
   async ({cep}) => {
     const url = `${API_BASE}/api/portifolio:list?filter=%7B%7D`;
-    const data = await makeRequest<Foods>(url, "GET");
+    const data = await makeRequest<Product>(url, "GET");
 
     if (!data) {
       return {
         content: [
           {
             type: "text",
-            text: "Não existe alimentos cadastrado para o CEP informado!",
+            text: "Não existe produto(s) para o CEP informado. Tente pesquisar o outro CEP!",
           },
         ],
       };
     }
 
-    var text: string = "Alimentos disponíveis:\n\n";
+    var text: string = `Produtos disponíveis para o CEP: ${cep}\n\n`;
 
     if (data.data && Array.isArray(data.data)) {
         data.data.forEach(food => {
-          text += `{ID: ${food.id}\n`;
+          text += `{ID_produto: ${food.id}\n`;
+          text += `Categoria do produto: ${food.category}\n`;
           text += `Título: ${food.title}\n`;
           text += `Descrição: ${food.description}\n`;
           text += `Preço: ${food.price}\n`;
@@ -137,21 +138,43 @@ server.tool(
   }
 );
 
+/**
+ * Tool to open a shopping cart for a given ZIP code (CEP).
+ * It requires the CEP to be specified.
+ */
 server.tool(
   "Abrir-Carrinho-Tool",
-  "Abrir carrinho. Dados necessários: cep de entrega.",
+  "Abrir carrinho para adicionar produtos. Dados necessários: cep de entrega.",
   {
     cep: z.string().trim(),
   },
   async ({ cep }) => {    
 
-    var text: string = "Carrinho aberto com sucesso!\n\n";
+    const body = {
+      title: `Pedido para entrega no CEP ${cep}`,
+      cep: cep,
+      status: "Aberto"
+    };
 
+    const url = `${API_BASE}/api/sales_order:create`;
+    const data = await makeRequest<SalesOrder>(url, "POST", body);
+
+    if (data && data.data) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: `Carrinho aberto com sucesso! O ID do pedido é ${data.data.id}. Agora você pode ver a listagem de produtos.\n\n`,
+          },
+        ],
+      };
+    }
+      
     return {
       content: [
         {
           type: "text",
-          text: text,
+          text: "Não foi possível abrir o carrinho. Tente novamente mais!",
         },
       ],
     };
@@ -159,12 +182,12 @@ server.tool(
 );
 
 /**
- * Tool to purchase food items available in the CRM.
- * It requires the food ID and quantity to be specified.
+ * Tool to add food items to the shopping cart.
+ * It requires the food ID, quantity, and order ID to be specified.
  */
 server.tool(
-  "Adicionar-Alimentos-Carrinho-Tool",
-  "Adicionar múltiplos alimentos. Dados necessários: array de itens com ID, quantidade e preço unitário, total do pedido e cep de entrega.",
+  "Adicionar-Produtos-Carrinho-Tool",
+  "Adicionar múltiplos produtos no carrinho já aberto. Dados necessários: array de itens com ID do produto, quantidade, e ID da ordem.",
   {
     items: z.array(z.object({
       food_id: z.number().min(1),
@@ -174,23 +197,58 @@ server.tool(
   },
   async ({ items, order_id }) => {    
 
+    if (items.length === 0 || order_id <= 0 ) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: "Não foi possivel adicionar produtos pois não foi informado os dados necessários. Tente novamente mais!",
+          },
+        ],
+      };
+    }
 
-    var text: string = "Alimentos adicionado no carrinho com sucesso!\n\n";
+    for (const item of items) {
+
+      const body = {
+        "sales_order_id": order_id,
+        "portifolio_id": item.food_id,
+        "quantity": item.quantity
+      };
+
+      const url = `${API_BASE}/api/order_portifolio:create`;
+      const data = await makeRequest<SalesOrder>(url, "POST", body);
+
+      if (!data) {
+        return {
+          content: [
+            {
+              type: "text",
+              text: "Não foi possível adicionar o produto no carrinho. Tente novamente mais!",
+            },
+          ],
+        };
+      } 
+    }
 
     return {
       content: [
         {
           type: "text",
-          text: text,
+          text: "Produto(s) adicionado(s) no carrinho com sucesso!\n\n",
         },
       ],
     };
   }
 );
 
+/**
+ * Tool to remove food items from the shopping cart.
+ * It requires an array of items with food ID and order ID to be specified.
+ */
 server.tool(
-  "Remover-Alimentos-Carrinho-Tool",
-  "Remover múltiplos alimentos. Dados necessários: array de itens com ID do alimento e ID da ordem.",
+  "Remover-Produtos-Carrinho-Tool",
+  "Remover múltiplos produtos. Dados necessários: array de itens com ID do alimento e ID da ordem.",
   {
     items: z.array(z.object({
       food_id: z.number().min(1),
@@ -199,19 +257,49 @@ server.tool(
   },
   async ({ items }) => {    
 
-    var text: string = "Segue o carrinho atualizado...!\n\n";
+    if (items.length === 0 ) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: "Não foi possivel remover produto(s) pois não foi informado os dados necessários. Tente novamente mais!",
+          },
+        ],
+      };
+    }
+
+    for (const item of items) {
+
+      const url = `${API_BASE}/api/order_portifolio:destroy?filter=%7B%0A%22sales_order_id%22%3A%20${item.order_id}%2C%0A%22portifolio_id%22%3A%20${item.food_id}%0A%7D`;
+      const data = await makeRequest<SalesOrder>(url, "POST");
+
+      if (!data) {
+        return {
+          content: [
+            {
+              type: "text",
+              text: "Não foi possível remover o produto no carrinho. Tente novamente mais!",
+            },
+          ],
+        };
+      } 
+    }
 
     return {
       content: [
         {
           type: "text",
-          text: text,
+          text: "Produto(s) adicionado(s) no carrinho com sucesso!\n\n",
         },
       ],
     };
   }
 );
 
+/**
+ * Tool to finalize the shopping cart.
+ * It requires the order ID to be specified.
+ */
 server.tool(
   "Finalizar-Carrinho-Tool",
   "Finalizar as compras no carrinho . Dados necessários: Id da Ordem de compra.",
@@ -220,13 +308,36 @@ server.tool(
   },
   async ({ order_id }) => {    
 
-    var text: string = "Carrinho finalizado com sucesso!\n\n";
+    if (order_id === 0 ) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: "Não foi possivel finalizar o carrinho pois não foi informado os dados necessários. Tente novamente mais!",
+          },
+        ],
+      };
+    }
+
+    const url = `${API_BASE}/api/sales_order:update?filterByTk=${order_id}`;
+    const data = await makeRequest<SalesOrder>(url, "POST", {status: "Finalizado"});
+
+    if (!data) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: "Não foi possível finalizar carrinho. Tente novamente mais!",
+          },
+        ],
+      };
+    } 
 
     return {
       content: [
         {
           type: "text",
-          text: text,
+          text: "Carrinho fechado com sucesso!\n\n",
         },
       ],
     };
